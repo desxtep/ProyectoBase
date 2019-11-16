bfd6c5222c3140699b715a5826efe97feff61b34
Manuel Antonio Batres Luna <al155192@edu.uaa.mx>
2019-11-15 20:01:01 -0600

    bases view model

---
+++ Base/Base/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base.ViewModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnProperetyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingFlied, T value, [CallerMemberName] string propertyName = null) {
+            if (EqualityComparer<T>.Default.Equals(backingFlied, value)) {
+                return;
+            }
+            backingFlied = value;
+            OnProperetyChanged(propertyName);
+        }
+    }
+}
--- Base/Base/ViewModels/LoginViewModel.cs
+++ Base/Base/ViewModels/LoginViewModel.cs
@@ -2,26 +2,41 @@
 
 namespace Base.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
+    using System.ComponentModel;
     using System.Windows.Input;
-    class LoginViewModel
+    using Xamarin.Forms;
+
+    class LoginViewModel : BaseViewModel
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnable;
+        #endregion
         #region Properties
-        public String Email {
+        public String Email
+        {
             get;
             set;
         }
 
         public String Password
         {
-            get;
-            set;
+            get { return password; }
+            set { SetValue(ref password, value); }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return isRunning; }
+            set { SetValue(ref isRunning, value); }
         }
 
         public bool IsRemembered
@@ -29,18 +44,75 @@ namespace Base.ViewModels
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get { return isEnable; }
+            set { SetValue(ref isEnable, value); }
+        }
         #endregion
 
         #region Commands
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+
         }
+
+
+
+        private async void Login()
+        {
+            if (String.IsNullOrEmpty(this.Email))
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter your email.",
+                    "Accept");
+                return;
+            }
+            if (String.IsNullOrEmpty(this.Password))
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter your password.",
+                    "Accept");
+                return;
+            }
+            this.IsRunning = true;
+            this.IsEnabled = false;
+            if (this.Email != "m" || this.Password != "asdf")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password is incorrect",
+                    "Accept");
+                this.Password = String.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                   "Ok",
+                   "Ready",
+                   "Accept");
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+        }
+
+
         #endregion
 
         #region Contructors
-        public LoginViewModel(){
+        public LoginViewModel()
+        {
             this.IsRemembered = true;
         }
         #endregion
